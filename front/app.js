4c3b34f5e3321887fd604a9c3e3ed5240e9f2692
MusabShakeel576 <musabshakeel576@gmail.com>
2021-04-22 00:28:29 +0500

    eigth commit

--- front/app.js
+++ front/app.js
@@ -1,6 +1,6 @@
 $( document ).ready(() => {
     // API requests
-    fetch('https://sourscarcountdown.herokuapp.com/admin.html', {
+    fetch('admin.html', {
       method: 'GET',
       mode: 'cors',
       headers: {
